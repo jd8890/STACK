8fc424fe6562ccbbf2e9b1c46a26c9a221b9178f
jd8890 <104278313+jd8890@users.noreply.github.com>
2022-10-26 13:56:46 +0530

    Create pop.c

---
+++ pop.c
@@ -0,0 +1,8 @@
+// Remove element from stack
+void pop(st *s) {
+  if (isempty(s)) {
+    printf("\n STACK EMPTY \n");
+  } else {
+    printf("Item popped= %d", s->items[s->top]);
+    s->top--;
+  }
